3c249d57eefc5e6c31b06e1e682d29ab5f9f2582
dumeng0801 <dumeng0801@gmail.com>
2024-10-17 09:07:11 +0000

    修改格式

---
+++ reformat.js
@@ -0,0 +1,21 @@
+import { readFile, writeFile } from 'fs/promises'
+import { resolve } from 'path'
+
+async function main() {
+    const dir = import.meta.dirname
+    const file = resolve(dir, '300.json')
+    const data = await readFile(file, 'utf8')
+    const poems = JSON.parse(data)
+    poems.forEach(p=> {
+        const contents = p.contents
+        let lines = contents.split('\n')
+        lines = lines.map( line => {
+            return line.replaceAll(/[。！？，]/g, ' ') .trim()
+            .replace(' ', '.')
+        })
+        p.contents = lines.join('|')
+    })
+    await writeFile(resolve(dir, 'poems.json'), JSON.stringify(poems, null, 2))
+}
+
+main()
